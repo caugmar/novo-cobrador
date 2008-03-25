99b443c7b0720af9e3780d22ba1c3167520ece9a
Carlos Augusto Marcicano <caugm@nargothrond.(none)>
2008-03-25 09:58:03 -0300

    Atualizações de março.
    committer: Carlos Augusto Marcicano <caugm@nargothrond.(none)>
    
    --HG--
    extra : convert_revision : 1b1eb3c72368828759c9c3980362db4dd4bbba86

--- configuracoes.py
+++ configuracoes.py
@@ -2,23 +2,21 @@
 
 # Dados de configuração dos recibos do escritório e da Agro Comercial Takafuji.
 
-vencimento = "29/02/2008"
-pagamento = "4 de março de 2008"
+vencimento = "31/03/2008"
+pagamento = "4 de abril de 2008"
 
 # Dados de configuração dos recibos de aluguel do prédio.
 
-mes_vcto = "FEVEREIRO/2008"
-vcto = "28/02/2008"
-pgto = "4 de março de 2008"
-mes_pgto = "março de 2008"
-iptu = "01/11"
+mes_vcto = "MARÇO/2008"
+vcto = "31/03/2008"
+pgto = "4 de abril de 2008"
+mes_pgto = "abril de 2008"
+iptu = "02/11"
 
 # Configurações específicas para Lídia Meire Barreto Lopes 
-lidia_mes_vcto = "DEZEMBRO/2007"
-lidia_vcto = "31/12/2007"
-pgto = "4 de março de 2008"
-iptu = "01/11"
+lidia_mes_vcto = "JANEIRO/2008"
+lidia_vcto = "31/01/2008"
 
 # Configurações específicas para LDG
-ldg_periodo_vcto = "20/02/2008 a 20/03/2008"
-ldg_vcto = "20/03/2008"
+ldg_periodo_vcto = "20/03/2008 a 20/04/2008"
+ldg_vcto = "20/04/2008"
