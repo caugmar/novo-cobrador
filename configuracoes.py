b95cd909cd3311e4b4ba6707fe5553a6868c7d24
Carlos Augusto Marcicano <caugm@caugmar.no-ip.org>
2008-06-05 11:20:31 -0300

    Atualizado para cobrança de maio/2008.
    committer: Carlos Augusto Marcicano <caugm@caugmar.no-ip.org>
    
    --HG--
    extra : convert_revision : aee36bfb08f8ed5428ee3b0d85ca33fdfaf790d2

--- configuracoes.py
+++ configuracoes.py
@@ -2,21 +2,21 @@
 
 # Dados de configuração dos recibos do escritório e da Agro Comercial Takafuji.
 
-vencimento = "30/04/2008"
-pagamento = "5 de maio de 2008"
+vencimento = "31/05/2008"
+pagamento = "4 de junho de 2008"
 
 # Dados de configuração dos recibos de aluguel do prédio.
 
-mes_vcto = "ABRIL/2008"
-vcto = "30/04/2008"
-pgto = "5 de maio de 2008"
-mes_pgto = "maio de 2008"
-iptu = "03/11"
+mes_vcto = "MAIO/2008"
+vcto = "31/05/2008"
+pgto = "4 de junho de 2008"
+mes_pgto = "junho de 2008"
+iptu = "04/11"
 
 # Configurações específicas para Lídia Meire Barreto Lopes 
-lidia_mes_vcto = "FEVEREIRO/2008"
-lidia_vcto = "28/02/2008"
+lidia_mes_vcto = "MARÇO/2008"
+lidia_vcto = "31/03/2008"
 
 # Configurações específicas para LDG
-ldg_periodo_vcto = "20/04/2008 a 20/05/2008"
-ldg_vcto = "20/05/2008"
+ldg_periodo_vcto = "20/05/2008 a 20/06/2008"
+ldg_vcto = "20/06/2008"
