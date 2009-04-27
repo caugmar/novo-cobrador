1201f71dfdc47c7d9c67a550382a759c6e251ef1
Carlos Augusto Marcicano <caugmar@gmail.com>
2009-04-27 16:40:46 -0300

    Alterações para a cobrança de abril/2009.
    committer: Carlos Augusto Marcicano <caugmar@gmail.com>
    
    --HG--
    extra : convert_revision : 8c0741238d469312fd0dc3fd758dd9e23c7d178b

--- cobrador.py
+++ cobrador.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2.4
+#!/usr/local/bin/python2.6
 # -*- coding: latin-1 -*-
 
 from comum import *
@@ -13,19 +13,19 @@ if __name__ == '__main__':
     global cp850
     parser = OptionParser("%prog [-c] [-g] [-e] [-d] [-r]")
     parser.add_option("-c", "--carregar", action="store_true", dest="carregar",
-                      help="carrega as planilhas no banco de dados")
+                      help=u"carrega as planilhas no banco de dados")
     parser.add_option("-g", "--gerar", action="store_true", dest="gerar",
-                      help="gera os documentos de cobrança")
+                      help=u"gera os documentos de cobrança")
     parser.add_option("-e", "--emitir", dest="emitir", metavar="EMISSAO",
-                      help="emite os documentos de cobrança referentes à data de EMISSAO")
+                      help=u"emite os documentos de cobrança referentes à data de EMISSAO")
     parser.add_option("-d", "--cp-850", action="store_true", dest="cp850",
-                      help="codifica para impressão em CP-850 (DOS)")
+                      help=u"codifica para impressão em CP-850 (DOS)")
     parser.add_option("-r", "--relatorios", dest="relatorios", metavar="EMISSAO",
-                      help="gera os relatórios de cobrança referentes à data de EMISSAO")
+                      help=u"gera os relatórios de cobrança referentes à data de EMISSAO")
     parser.add_option("-a", "--atualizar-planilhas", dest="atualizar", metavar="EMISSAO",
-                      help="atualiza as planilhas dos clientes com os documentos referentes à data de EMISSAO")
+                      help=u"atualiza as planilhas dos clientes com os documentos referentes à data de EMISSAO")
     parser.add_option("-x", "--eliminar", dest="eliminar", metavar="EMISSAO",
-                      help="remove do banco de dados os documentos referentes à data de EMISSAO")
+                      help=u"remove do banco de dados os documentos referentes à data de EMISSAO")
     (options, args) = parser.parse_args()
     if options.carregar: 
         carregar()
